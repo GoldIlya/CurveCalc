db7a90825bdf58262ed402b811dcda0dc4cfae26
GoldIlya <Kurbatov-work@mail.ru>
2024-07-11 01:10:27 +1000

    Добавляем экспорт (переделать в верх меню)

--- app/src/main/java/ru/itinbiz/curvecalc/SeriesTableActivity.java
+++ app/src/main/java/ru/itinbiz/curvecalc/SeriesTableActivity.java
@@ -1,32 +1,57 @@
 package ru.itinbiz.curvecalc;
 
+import android.Manifest;
+import android.content.ContentResolver;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Looper;
+import android.os.ParcelFileDescriptor;
 import android.util.TypedValue;
 import android.view.Gravity;
 import android.view.MotionEvent;
 import android.view.ScaleGestureDetector;
 import android.view.View;
+import android.webkit.MimeTypeMap;
+import android.widget.Button;
 import android.widget.FrameLayout;
 import android.widget.HorizontalScrollView;
 import android.widget.ScrollView;
 import android.widget.TableLayout;
 import android.widget.TableRow;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.constraintlayout.widget.ConstraintLayout;
+import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
 
 import com.androidplot.xy.SimpleXYSeries;
 import com.google.common.reflect.TypeToken;
 import com.google.gson.Gson;
 
+import org.apache.poi.hssf.usermodel.HSSFWorkbook;
+import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.CellStyle;
+import org.apache.poi.ss.usermodel.Font;
+import org.apache.poi.ss.usermodel.IndexedColors;
+import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.ss.usermodel.Sheet;
+
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.OutputStream;
 import java.lang.reflect.Type;
 import java.util.ArrayList;
 import java.util.List;
 
 public class SeriesTableActivity extends AppCompatActivity {
+    private static final int REQUEST_PERMISSIONS = 1;
+    private static final int REQUEST_CODE_CREATE_DOCUMENT = 100;
+
     private List<SimpleXYSeries> seriesList = new ArrayList<>();
     private TableLayout seriesTable;
     private float scaleFactor = 1.0f;
@@ -34,6 +59,11 @@ public class SeriesTableActivity extends AppCompatActivity {
     private FrameLayout frameLayout;
     private HorizontalScrollView horizontalScrollView;
     private ScrollView scrollView;
+    private Button btnExcel;
+    HSSFWorkbook workbook = new HSSFWorkbook();
+    Sheet sheet = workbook.createSheet("Sheet1");
+
+    boolean boolean_permission;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -46,7 +76,8 @@ public class SeriesTableActivity extends AppCompatActivity {
         Gson gson = new Gson();
         Type seriesListType = new TypeToken<ArrayList<SimpleXYSeries>>() {}.getType();
         seriesList = gson.fromJson(seriesListJson, seriesListType);
-
+        fn_permission();
+        btnExcel = findViewById(R.id.btnExcel);
         // Initialize UI elements
         seriesTable = new TableLayout(this);
 
@@ -67,6 +98,17 @@ public class SeriesTableActivity extends AppCompatActivity {
 
         // Create the table
         createTable(seriesTable, seriesList);
+
+        btnExcel.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (boolean_permission) {
+                    Toast.makeText(SeriesTableActivity.this, "Excell", Toast.LENGTH_SHORT).show();
+                    createAndSaveExcelTable();
+                    openPath();
+                }
+            }
+        });
 
         // Initialize gesture detectors
         scaleGestureDetector = new ScaleGestureDetector(this, new ScaleGestureDetector.SimpleOnScaleGestureListener() {
@@ -114,6 +156,9 @@ public class SeriesTableActivity extends AppCompatActivity {
         frameLayout.setOnTouchListener(touchListener);
         horizontalScrollView.setOnTouchListener(touchListener);
         scrollView.setOnTouchListener(touchListener);
+
+
+
     }
 
     private void updatePaddingAndGravity() {
@@ -218,5 +263,115 @@ public class SeriesTableActivity extends AppCompatActivity {
             }
         }
         return differenceSeries;
+    }
+
+    private void fn_permission() {
+        if ((ContextCompat.checkSelfPermission(getApplicationContext(), Manifest.permission.READ_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED)||
+                (ContextCompat.checkSelfPermission(getApplicationContext(), Manifest.permission.WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED)) {
+
+
+            if ((ActivityCompat.shouldShowRequestPermissionRationale(SeriesTableActivity.this, android.Manifest.permission.READ_EXTERNAL_STORAGE))) {
+            } else {
+                ActivityCompat.requestPermissions(SeriesTableActivity.this, new String[]{android.Manifest.permission.READ_EXTERNAL_STORAGE},
+                        REQUEST_PERMISSIONS);
+
+            }
+
+            if ((ActivityCompat.shouldShowRequestPermissionRationale(SeriesTableActivity.this, Manifest.permission.WRITE_EXTERNAL_STORAGE))) {
+            } else {
+                ActivityCompat.requestPermissions(SeriesTableActivity.this, new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE},
+                        REQUEST_PERMISSIONS);
+
+            }
+        } else {
+            boolean_permission = true;
+
+        }
+    }
+
+
+    private void createAndSaveExcelTable() {
+
+        Row headerRow = sheet.createRow(0);
+        Cell xHeader = headerRow.createCell(0);
+        xHeader.setCellValue("Номер");
+        for (int i = 0; i < seriesList.size(); i++) {
+            Cell yHeader = headerRow.createCell(i + 1);
+            if (i == 0) {
+                yHeader.setCellValue("Промер");
+            } else {
+                yHeader.setCellValue("Шаг " + i);
+            }
+        }
+
+        // Add table rows for each data point in the seriesList
+        int numRows = seriesList.get(0).size();
+        for (int i = 0; i < numRows; i++) {
+            Row dataRow = sheet.createRow(i + 1);
+            Cell yValue = dataRow.createCell(0);
+            boolean isInteger = (seriesList.get(0).getY(i).doubleValue() - Math.floor(seriesList.get(0).getY(i).doubleValue())) == 0;
+            if (isInteger) {
+                yValue.setCellValue(seriesList.get(0).getY(i).doubleValue());
+
+            } else {
+                yValue.setCellValue(" - ");
+
+            }
+            for (int j = 0; j < seriesList.size(); j++) {
+                Cell xValue = dataRow.createCell(j + 1);
+                if (j > 0) {
+                    SimpleXYSeries diff = calculateDifference(seriesList.get(j - 1), seriesList.get(j), j);
+                    Double iDiff = diff.getX(i).doubleValue();
+                    if (iDiff!= 0.0) {
+                        if (iDiff > 0) {
+                            xValue.setCellValue(seriesList.get(j).getX(i).doubleValue() + "  +" + iDiff);
+
+                        } else {
+                            xValue.setCellValue(seriesList.get(j).getX(i).doubleValue() + "  " + iDiff);
+
+                        }
+                    } else {
+                        xValue.setCellValue(seriesList.get(j).getX(i).doubleValue());
+                    }
+                } else {
+                    xValue.setCellValue(seriesList.get(j).getX(i).doubleValue());
+                }
+            }
+        }
+
+    }
+
+
+
+    private void openPath() {
+        // write the document content
+        String fileName = "test.xls";
+        ContentResolver contentResolver = getContentResolver();
+        MimeTypeMap mimeTypeMap = MimeTypeMap.getSingleton();
+        String mimeType = mimeTypeMap.getMimeTypeFromExtension("xls");
+        Intent intent = new Intent(Intent.ACTION_CREATE_DOCUMENT);
+        intent.setType(mimeType);
+        intent.putExtra(Intent.EXTRA_TITLE, fileName);
+        startActivityForResult(intent, REQUEST_CODE_CREATE_DOCUMENT);
+    }
+
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == REQUEST_CODE_CREATE_DOCUMENT && resultCode == RESULT_OK) {
+            Uri uri = data.getData();
+            if (uri != null) {
+                try (ParcelFileDescriptor pfd = getContentResolver().openFileDescriptor(uri, "w");
+                     FileOutputStream fos = new FileOutputStream(pfd.getFileDescriptor())) {
+                    // Write the document content to the file
+                    workbook.write(fos);
+                    workbook.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                    Handler handler = new Handler(Looper.getMainLooper());
+                }
+            }
+        }
     }
 }
