b8fcd05a2a235f3faad2db45a5071888892f58ff
GoldIlya <Kurbatov-work@mail.ru>
2024-07-18 10:52:15 +1000

    Публикация версии 1.1 экспорт PDF и Excel

--- app/src/main/java/ru/itinbiz/curvecalc/InfoActivity.java
+++ app/src/main/java/ru/itinbiz/curvecalc/InfoActivity.java
@@ -1,6 +1,10 @@
 package ru.itinbiz.curvecalc;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -10,5 +14,25 @@ public class InfoActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_info);
+
+        Button telegramButton = findViewById(R.id.btnTelega);
+        Button websiteButton = findViewById(R.id.btnUpdate);
+
+        telegramButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse("https://t.me/Relzcontract"));
+                startActivity(intent);
+            }
+        });
+
+        websiteButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse("https://apps.rustore.ru/app/ru.itinbiz.curvecalc"));
+                startActivity(intent);
+            }
+        });
     }
-}
+
+    }
